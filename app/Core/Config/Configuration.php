fb658312de8674c51c213754ff87d7fc60763be2
djeday <gvozdetskyidev@gmail.com>
2021-04-07 13:18:58 +0300

    Add database connection

--- app/Core/Config/Configuration.php
+++ app/Core/Config/Configuration.php
@@ -6,6 +6,14 @@ class Configuration
 {
     private string $templateDir;
 
+    private string $dbHost;
+
+    private string $dbName;
+
+    private string $dbUser;
+
+    private string $dbPass;
+
     public function getTemplateDir(): string
     {
         return $this->templateDir;
@@ -14,5 +22,45 @@ class Configuration
     public function setTemplateDir(string $templateDir): void
     {
         $this->templateDir = $templateDir;
+    }
+
+    public function getDbHost(): string
+    {
+        return $this->dbHost;
+    }
+
+    public function setDbHost(string $dbHost): void
+    {
+        $this->dbHost = $dbHost;
+    }
+
+    public function getDbName(): string
+    {
+        return $this->dbName;
+    }
+
+    public function setDbName(string $dbName): void
+    {
+        $this->dbName = $dbName;
+    }
+
+    public function getDbUser(): string
+    {
+        return $this->dbUser;
+    }
+
+    public function setDbUser(string $dbUser): void
+    {
+        $this->dbUser = $dbUser;
+    }
+
+    public function getDbPass(): string
+    {
+        return $this->dbPass;
+    }
+
+    public function setDbPass(string $dbPass): void
+    {
+        $this->dbPass = $dbPass;
     }
 }
